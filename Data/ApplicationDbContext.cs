59fc79b0bbf60d926ecd73b17eb69e0d59a5e22e
Toluwalase Aro <arodiana2019@gmail.com>
2024-11-25 11:03:16 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Auctions.Models;
+
+namespace Auctions.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Auctions.Models.Listing> Listing { get; set; } = default!;
+    }
+}
